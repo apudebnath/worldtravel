8373ceb3b64b38826cd7c2dfd348d117551dcde0
apudebnath <apu1578@gmail.com>
2022-01-26 05:08:49 +0600

    worldtravel first commit

---
+++ src/components/firebase/Firebase.init.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+const initAuthentication =() => {
+    initializeApp(firebaseConfig);
+}
+
+export default initAuthentication;
